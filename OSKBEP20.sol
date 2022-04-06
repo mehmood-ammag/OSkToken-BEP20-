6f79b22df1b035f2b6f9386fac23cc54d99d4021
mehmood-ammag <mehmood.asghar@ammag.tech>
2022-04-05 23:37:01 -0700

    Update OSKBEP20.sol

--- OSKBEP20.sol
+++ OSKBEP20.sol
@@ -5,12 +5,11 @@ import "./ERC20.sol";
 import "./Ownable.sol";
 
 
-contract OSkToken is ERC20 , Ownable {
+contract OSKToken is ERC20 , Ownable {
 
-    uint256 private _mintLimit = 200000000;
     constructor(address masterWallet) ERC20("OSK Token", "OSK") Ownable(masterWallet) {
 
-        require(masterWallet != address(0),"OSkToken: masterWallet is the zero address");
-        _mint(masterWallet,_mintLimit*10**uint256(decimals()));            
+        require(masterWallet != address(0),"OSKToken: masterWallet is the zero address");
+        _mint(masterWallet,2*10**26);            
     }
 }
